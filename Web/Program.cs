0f29ce7408acf9ebb7174adc8e835f750e5364cd
Robin Avery <robin@ribbanya.dev>
2021-08-04 04:40:43 -0400

    Create `.sln` and `Web.csproj`

---
+++ Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeleePeachRD.PrincessDiaries.Web
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
